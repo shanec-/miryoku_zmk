db131cded44b953be425db850c60d1b7a057335c
shane carvalho <shanec_@hotmail.com>
2024-09-09 00:22:06 +1000

    adjusted tapping term

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -19,6 +19,7 @@ U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp 
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, RET),U_LT(U_MOUSE, TAB),U_LT(U_SYM, SPACE),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
+#define U_TAPPING_TERM 250
 
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 39 40
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 41 42
